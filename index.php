8d3217dc195947fe3192a71a6b9b80ba2cb566cf
yutaoli <1213617849@qq.com>
2015-07-01 09:02:30 +0800

    运行环境OK

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=GB2312">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo "编号" ."书名"
+        ?>
+    </body>
+</html>

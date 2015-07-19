f3c553429d5f273a9a7ccf15aeee8aa60235cca6
yutaoli <1213617849@qq.com>
2015-07-19 11:30:27 +0800

    新增review表的“已复习”功能。
    下面要处理
    1、BookService、ReviewService的增删改查 参数全部是Book、或Review，而非现在的零散参数如user_id, book_id
    2、user表的CURD。

--- list_review.php
+++ list_review.php
@@ -7,6 +7,12 @@ and open the template in the editor.
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=GB2312">
         <title></title>
+        <script  type="text/javascript">
+            function confirmDel( val)
+            {
+                return window.confirm("真的要删除《"+val+"》?");
+            }
+        </script>
     </head>
     <body>
         <?php
@@ -28,7 +34,7 @@ and open the template in the editor.
         echo "<table border='1px' bordercolor='green' celspacing='0px' width='700px'> ";
         echo "<tr>";
         echo "<th>编号</th><th>user_id</th><th>book_id</th><th>book_name</th><th>left_days_next_review</th><th>last_review</th>";
-        echo "<th>times_reviewed</th><th>note</th><th>删除</th><th>修改</th>";
+        echo "<th>times_reviewed</th><th>note</th><th>已复习</th><th>删除</th><th>修改</th>";
         echo "</tr>";
 
         //复习循环周期，如第一次复习是第二天，即间隔一天，第二次复习是第一次复习的7天后，如此类推。。
@@ -82,7 +88,21 @@ and open the template in the editor.
                 echo "<td>{$num}</td><td>{$row['user_id']}</td><td>{$row['book_id']}</td><td>{$row['book_name']}</td><td><font color='#FF0000' size=5>{$left_days_next_review}</font></td><td>{$date_last_review}</td>";
             }
             echo "<td>{$row['times_reviewed']}</td><td>{$row['note']}</td>";
-            echo "<td><a onclick='return confirmDel(\"{$row['book_id']}\")' href='hgi_del_review.php?user_id={$row['user_id']}&book_id={$row['book_id']}'>删除</a></td>";
+
+            //已复习，用POST提交
+            $next_times_reviewed = $row['times_reviewed'] + 1;
+            echo "<td>";
+            echo "<form action='hgi_modify_review.php' method='post' name='form_hgi_modify_review{$num}'>";
+            echo "<input type='hidden' name='user_id' value='{$row['user_id']}' />";
+            echo "<input type='hidden' name='book_id' value='{$row['book_id']}' />";
+            echo "<input type='hidden' name='last_review' value='{$time_stamp_now}'  />";
+            echo "<input type='hidden' name='times_reviewed' value='{$next_times_reviewed}' />";
+            echo "<input type='hidden' name='note' value='{$row['note']}' />";
+            echo "<a href='javascript:document.form_hgi_modify_review{$num}.submit()'>已复习</a>";
+            echo "</form>";
+            echo "</td>";
+
+            echo "<td><a onclick='return confirmDel(\"{$row['book_name']}\")' href='hgi_del_review.php?user_id={$row['user_id']}&book_id={$row['book_id']}'>删除</a></td>";
             echo "<td><a  href='modify_review.php?user_id={$row['user_id']}&book_id={$row['book_id']}&book_name={$row['book_name']}'>修改</a></td>";
             echo "</tr>";
         }
